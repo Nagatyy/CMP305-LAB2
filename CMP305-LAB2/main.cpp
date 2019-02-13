b9738e78a48afe0d2e161eb5e97be06df11698c8
Nagatyy <mohamednagaty@aucegypt.edu>
2019-02-13 11:38:35 +0400

    Initial Commit

---
+++ CMP305-LAB2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CMP305-LAB2
+//
+//  Created by Mohamed Nagaty on 2/13/19.
+//  Copyright © 2019 Mohamed Nagaty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
